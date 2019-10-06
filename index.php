18f142bb2c356c3489a1d62f4e8bfe186fe63075
Fuangwith-Bkk <51965769+Fuangwith-Bkk@users.noreply.github.com>
2019-10-06 21:32:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,16 @@
   for ($i=1;$i<=5;$i++){
     echo "Hello --> $i <br>";
   }
+
+  echo "<hr>";
+  $namespace = getenv('OPENSHIFT_BUILD_NAMESPACE');
+  $buildname = getenv('OPENSHIFT_BUILD_NAME=web-7');
+  $github = getenv('OPENSHIFT_BUILD_SOURCE');
+  $src_folder = getenv('HOME');
+  $hostname = getenv('HOSTNAME');
+
+  echo "This page is running on $hostname.<br>";
+  echo "Pod $hostname is in $namespace project and was built from $github. <br>";
+
   phpinfo();
 ?>
